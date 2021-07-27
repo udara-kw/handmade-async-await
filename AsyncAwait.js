66a5139281f7bcab0566e68a717968fb5123c9ae
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-07-27 12:39:58 +0530

    Fix errors

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,15 +1,15 @@
 const fetch = require("node-fetch");
 
-function doWhenDataReceived(data) {
-  returnNextElement.next();
+function doWhenDataReceived(value) {
+  returnNextElement.next(value);
 }
 
-function *createFlow() {
-  const data = yield fetch("https://twitter.com/JavaScript/status/1329470364401819649");
+function* createFlow() {
+  const data = yield fetch(
+    "https://twitter.com/JavaScript/status/1329470364401819649"
+  ).then(doWhenDataReceived);
   console.log(data);
 }
 
 const returnNextElement = createFlow();
 const futureData = returnNextElement.next();
-console.log(futureData);
-//futureData.then(doWhenDataReceived);
